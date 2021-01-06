4b48253978cf949e24596a4b7b19e63bd72520a3
Mahmoud Ragab <mahmoudragab68@gmail.com>
2021-01-06 03:45:22 +0200

    add cors

--- index.js
+++ index.js
@@ -7,6 +7,28 @@ var socketIO = require('socket.io');
 
 var fileServer = new(nodeStatic.Server)();
 var app = http.createServer(function(req, res) {
+  const headers = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "OPTIONS, POST, GET",
+    "Access-Control-Max-Age": 2592000, // 30 days
+    /** add other headers as per requirement */
+  };
+
+  if (req.method === "OPTIONS") {
+    res.writeHead(204, headers);
+    res.end();
+    return;
+  }
+
+  if (["GET", "POST"].indexOf(req.method) > -1) {
+    res.writeHead(200, headers);
+    res.end("Hello World");
+    return;
+  }
+
+  res.writeHead(405, headers);
+  res.end(`${req.method} is not allowed for the request.`);
+
   fileServer.serve(req, res);
 }).listen(process.env.PORT || 8000);
 
